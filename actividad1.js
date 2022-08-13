5f8adae20b039f48d126bcb46d543d5bc6b129d7
Juan Pablo Figini <jpfigini@gmail.com>
2022-08-13 18:33:13 -0300

    subo actividad1

---
+++ actividad1.js
@@ -0,0 +1,15 @@
+<script>
+/* Crear tres (3) variables para asignar el nombre, 
+apellido y edad de Homero, respectivamente. 
+Es indistinto el empleo de var o  let en la declaración.
+*/
+
+var nombre = "Homero";
+var apellido = "Simpson";
+var edad = 10;
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+
+</script>
