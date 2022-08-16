b390b0732cfabf586bd0896d04cedc9d790b0c1f
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-08-16 11:06:15 -0300

    subo actividad 4

---
+++ actividad4.js
@@ -0,0 +1,13 @@
+/*
+Solicitar al usuario cinco (5) nombres pertenecientes a integrantes de una familia. 
+Luego, concatenar dichas entradas y efectuar una salida por alerta
+*/
+
+let familiar1 = prompt("ingresar familiar 1");
+let familiar2 = prompt("ingresar familiar 2");
+let familiar3 = prompt("ingresar familiar 3");
+let familiar4 = prompt("ingresar familiar 4");
+let familiar5 = prompt("ingresar familiar 5");
+
+console.log("Grupo familiar compuesto por:" + familiar1 + ", " + familiar2 + ", " + familiar3 + ", " + familiar4 + " y " + familiar5);
+
