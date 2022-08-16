16d2cf4df7035eee0aaaa1b71a8136e79168729f
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-08-16 10:25:55 -0300

    "subo actividad3"

---
+++ actividad3.js
@@ -0,0 +1,14 @@
+/*
+Crear cinco (5) variables constantes, asignando a cada una de ellas diferentes nombres de ciudades
+*/
+<script>
+
+const ciudad1 = "San Luis";
+const ciudad2 = "Villa Mercedes";
+const ciudad3 = "Merlo";
+const ciudad4 = "Juana Koslay";
+const ciudad5 = "Concaran";
+
+
+
+</script>
