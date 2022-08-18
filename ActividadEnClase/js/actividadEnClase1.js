d49c90a2b3ae47e848a86afcea6f32f3b0eb6017
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-08-18 11:17:23 -0300

    subo la actividadEnClase1 temrinada

--- ActividadEnClase/js/actividadEnClase1.js
+++ ActividadEnClase/js/actividadEnClase1.js
@@ -1,7 +1,11 @@
-if (3 >= 2) {
-    console.log("iguales");
-}
 
-else {
-    console.log("distintos");
-}
+let numero1 = prompt("Ingrese el primer número");
+let numero2 = prompt("Ingrese el segundo número");
+
+let suma = Number(numero1) + Number(numero2);
+let resta = numero1 - numero2;
+let multi = numero1 * numero2;
+
+alert(numero1 + " + " + numero2 + " = " + suma );
+alert(numero1 + " - " + numero2 + " = " + resta );
+alert(numero1 + " * " + numero2 + " = " + multi );2
