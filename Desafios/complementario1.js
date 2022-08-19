a7ddd0216845a5ace3639355075b577d0d888f2e
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-08-19 12:10:38 -0300

    subo estructura de Desafios

---
+++ Desafios/complementario1.js
@@ -0,0 +1,6 @@
+/*
+La CONSIGNA PARA ESTE DESAFÍO es enviar un ejercicio en el cual utilicen un ciclo de iteración que funcione correctamente. 
+No tiene que ser nada complejo. Por ciclo de iteración entendemos FOR, WHILE y DO-WHILE. El if y el switch los pueden incluir, 
+pero teniendo en cuenta que son condicionales, no basta con incluir eso solo si no hay ningún ciclo de iteración.
+*/
+
