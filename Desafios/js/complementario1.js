722b0b97043f6d03e98a82b32f6b8ec86b4ebdd4
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-08-23 20:17:47 -0300

    Agrego comentarios a cada sección

--- Desafios/js/complementario1.js
+++ Desafios/js/complementario1.js
@@ -7,16 +7,18 @@ pero teniendo en cuenta que son condicionales, no basta con incluir eso solo si 
 DESARROLLO
 Carga de listado de materiales para presupuesto de reparación de un vehículo. El usuario debe ingresar el número de presupuesto,
 nombre del cliente y la cantidad, descripción y costo de los repuestos a utilizar.
-"/n" 
 */
 
+//Se piden los datos del presupuesto
 let numPresup = "Presupuesto N°" + prompt("Ingrese número de presupuesto:");
 let nomCliente = "Nombre del Cliente: " + prompt("Ingrese nombre del Cliente:");
 let cantRep = Number(prompt("Ingrese la cantidad de repuestos:"));
 
+//declaro variables para cantidad y listado de repuestos
 let repuestos = "";
 let total = 0;
 
+//Declaro un bucle mediante la sentencia "for" para cargar los repuestos y su correspondiente precio
 for(let index=0; index < cantRep; index++){
 repuestos = repuestos + prompt("Ingrese repuesto N°" + (index+1));
 let precio = Number(prompt("Ingrese precio del repuesto N°" + (index+1)+" $:"));
@@ -24,4 +26,5 @@ repuestos = repuestos + " $" + precio + "\n";
 total = total + precio;
 }
 
+//Muestro por pantalla los datos del presupuesto en forma ordenada.
 alert(numPresup + "\n" + nomCliente + "\n" + "Repuestos:" + "\n" + repuestos + "\n" + "Total: $" + total);
