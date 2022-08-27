0bb50bceed1bd9ef0975069a5d4d441db63d2f02
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-08-26 22:06:28 -0300

    subo estructura de desafio entregable 1

---
+++ Desafios/js/entregable1.js
@@ -0,0 +1,8 @@
+/*
+DESAFÍO ENTREGABLE N° 1
+Compuesto por…
+Crear un algoritmo con un condicional.
+Crear un algoritmo utilizando un ciclo.
+Armar un simulador interactivo, la estructura final de tu proyecto integrador.
+
+*/
