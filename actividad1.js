136ff0e2e2854e427a847662457e7b86f7fe5c9b
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-09-01 08:44:37 -0300

    estructuras

---
+++ actividad1.js
@@ -0,0 +1,16 @@
+/*
+
+*/
+
+
+function persona1 (name,surname, adress) {
+
+this.nombre = name;
+this.apellido = surname;
+domicilio : adress;
+this.saludar = function () {
+    console.log("Hola mi nombre es:" + this.nombre);
+
+}
+}
+
