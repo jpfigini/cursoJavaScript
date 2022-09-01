4b3c20eaf6282c634ca7341cc25e45c40d0c9762
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-09-01 13:55:54 -0300

    se agregan 2 array y modifican las funciones para manjarlos

--- Desafios/js/complementario2.js
+++ Desafios/js/complementario2.js
@@ -1,9 +1,8 @@
 /*
-DESAFÍO ENTREGABLE N° 1
-Compuesto por…
-Crear un algoritmo con un condicional.
-Crear un algoritmo utilizando un ciclo.
-Armar un simulador interactivo, la estructura final de tu proyecto integrador.
+
+DESAFÍO COMPLEMENTARIO N° 2:
+DESAFÍO COMPLEMENTARIO EXPIRA EL MARTES 06/09/2022 23:59HS
+Desafío: Incorporar Arrays
 
 */
 
@@ -14,27 +13,35 @@ let cantRep = 0;
 let repuestos = "";
 let total = 0;
 let continua = "";
+let presupuesto = [];
+let repuestosArray = [];
 
 //Declaro una función para iniciar un nuevo presupuesto
 function iniciaPresupuesto (){
 //Se piden los datos del presupuesto
-numPresup = "Presupuesto N°" + prompt("Ingrese número de presupuesto:");
-nomCliente = "Nombre del Cliente: " + prompt("Ingrese nombre del Cliente:");
+presupuesto.push("Presupuesto N°" + prompt("Ingrese número de presupuesto:"));
+presupuesto.push("Nombre del Cliente: " + prompt("Ingrese nombre del Cliente:"));
 cantRep = Number(prompt("Ingrese la cantidad de repuestos:"));
 }
 
 //Declaro una función para imprimir un presupuesto
 function imprimePresupuesto (numPresup,nomCliente,repuestos,total){
-    alert(numPresup + "\n" + nomCliente + "\n" + "Repuestos:" + "\n" + repuestos + "\n" + "Total: $" + total);
+var imprimePresup = presupuesto[0] + "\n" + presupuesto[1] + "\n" + "Repuestos:";
+
+for(let index=0; index < repuestosArray.length; index+2){
+imprimePresup = repuestosArray[index] + repuestosArray[index+1]   + "\n" ;
+}
+imprimePresup = imprimePresup + "Total: $" + total;
+alert(imprimePresup);
 }
 
 //Declaro un función para la carga de los productos del presupuesto
 function cargaPresupuesto (){
 //Declaro un bucle mediante la sentencia "for" para cargar los repuestos y su correspondiente precio
 for(let index=0; index < cantRep; index++){
-repuestos = repuestos + prompt("Ingrese repuesto N°" + (index+1));
+repuestosArray.push(prompt("Ingrese repuesto N°" + (index+1)));
 let precio = Number(prompt("Ingrese precio del repuesto N°" + (index+1)+" $:"));
-repuestos = repuestos + " $" + precio + "\n";
+repuestosArray.push(precio);
 total = total + precio;
     }
 }
@@ -50,8 +57,8 @@ cargaPresupuesto();
 //LLamado a la función especifica para mostrar por pantalla los datos del presupuesto en forma ordenada.
 imprimePresupuesto(numPresup,nomCliente,repuestos,total);
 
-repuestos = "";
-precio = "";
+presupuesto = [];
+repuestosArray = [];
 total = 0;
 
 } while ("No" != (continua = prompt("Desea cargar otro presupuesto: Si/No:")))
