fc6c297fe67fdd0a3db90b92809091e627ed3d87
jpfigini <80461781+jpfigini@users.noreply.github.com>
2022-09-05 11:08:08 -0300

    corrección de comentarios em comp2

--- Desafios/js/complementario2.js
+++ Desafios/js/complementario2.js
@@ -7,10 +7,8 @@ Desafío: Incorporar Arrays
 */
 
 //declaro variables goblales
-let numPresup = 0;
-let nomCliente = "";
+
 let cantRep = 0;
-let repuestos = "";
 let total = 0;
 let continua = "";
 let presupuesto = [];
@@ -19,14 +17,14 @@ let imprimePresup = "";
 
 //Declaro una función para iniciar un nuevo presupuesto
 function iniciaPresupuesto (){
-//Se piden los datos del presupuesto
+//Se piden los datos del presupuesto y se cargan en un array
 presupuesto.push("Presupuesto N°" + prompt("Ingrese número de presupuesto:"));
 presupuesto.push("Nombre del Cliente: " + prompt("Ingrese nombre del Cliente:"));
 cantRep = Number(prompt("Ingrese la cantidad de repuestos:"));
 }
 
 //Declaro una función para imprimir un presupuesto
-function imprimePresupuesto (numPresup,nomCliente,repuestos,total){
+function imprimePresupuesto (){
 imprimePresup = presupuesto[0] + "\n" + presupuesto[1] + "\n" + "Repuestos:" + "\n";
 
 for(let index=0; index < repuestosArray.length; index = index+2){
@@ -40,7 +38,7 @@ alert(imprimePresup);
 
 //Declaro un función para la carga de los productos del presupuesto
 function cargaPresupuesto (){
-//Declaro un bucle mediante la sentencia "for" para cargar los repuestos y su correspondiente precio
+//Declaro un bucle mediante la sentencia "for" para cargar los repuestos y su correspondiente precio dentro de un array
 for(let index=0; index < cantRep; index++){
 repuestosArray.push(prompt("Ingrese repuesto N°" + (index+1)));
 let precio = Number(prompt("Ingrese precio del repuesto N°" + (index+1)+" $:"));
@@ -58,7 +56,7 @@ iniciaPresupuesto();
 cargaPresupuesto();
 
 //LLamado a la función especifica para mostrar por pantalla los datos del presupuesto en forma ordenada.
-imprimePresupuesto(numPresup,nomCliente,repuestos,total);
+imprimePresupuesto();
 
 presupuesto = [];
 repuestosArray = [];
